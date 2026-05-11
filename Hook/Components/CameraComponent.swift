196f5f62f5a3f9d7f108422dc1165bcf243eb4a7
Miranda <mirandautami3@gmail.com>
2026-05-11 21:19:46 +0700

    feat: add CameraComponent to manage SpriteKit camera node integration

--- Hook/Components/CameraComponent.swift
+++ Hook/Components/CameraComponent.swift
@@ -5,4 +5,19 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+import SpriteKit
+
+class CameraComponent: GKComponent {
+   
+    let cameraNode: SKCameraNode
+    
+    init(camera: SKCameraNode) {
+        self.cameraNode = camera
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
