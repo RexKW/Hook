300cd8ec4df5eb26d4bc9df50fa9b700b8befa65
Miranda <mirandautami3@gmail.com>
2026-05-11 21:16:35 +0700

    feat: add GameplayKit StateComponent for managing hook states including idle, casting, waiting, reeling, and captured

--- Hook/Components/StateComponent.swift
+++ Hook/Components/StateComponent.swift
@@ -5,4 +5,12 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+
+class StateComponent: GKComponent {
+    var currentState: HookState = .idle
+}
+
+enum HookState {
+    case idle, casting, waiting, reeling, captured
+}
