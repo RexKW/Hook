2de3d5516690dae3dc8fe9b43be12d6274cfd294
Miranda <mirandautami3@gmail.com>
2026-05-11 21:18:05 +0700

    feat: add MovementComponent with hook movement properties including speed, return speed, bottom limit, and surface position

--- Hook/Components/MovementComponent.swift
+++ Hook/Components/MovementComponent.swift
@@ -5,4 +5,14 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+import SpriteKit
+
+class MovementComponent: GKComponent {
+    // Data/Atribut (Ini yang dipakai oleh State nanti)
+    var speed: CGFloat = 7.0
+    var returnSpeed: CGFloat = 15.0
+    var bottomLimit: CGFloat = -3000
+    var surfaceY: CGFloat = 20
+    
+}
