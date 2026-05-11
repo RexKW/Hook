f77b945968c86f5b0a8f573225432dc77f37f78f
Rex Kenny Wirasantoso <153161558+RexKW@users.noreply.github.com>
2026-05-11 12:08:30 +0700

    feat: -added fish collection to ui -added fish detail to ui -added progression to ui

---
+++ Hook/UI/FishCollection.swift
@@ -0,0 +1,18 @@
+//
+//  FishCollection.swift
+//  Hook
+//
+//  Created by Rex Kenny Wirasantoso on 11/05/26.
+//
+
+import SwiftUI
+
+struct FishCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FishCollection()
+}
