1fadffc9390d7d268b7901a95dec9d13110d72da
Miranda <mirandautami3@gmail.com>
2026-05-14 22:38:28 +0700

    refactor: simplify MovementComponent with dedicated drop and reel speed properties

--- Hook/Components/MovementComponent.swift
+++ Hook/Components/MovementComponent.swift
@@ -6,16 +6,8 @@
 //
 
 import GameplayKit
-import SpriteKit
 
 class MovementComponent: GKComponent {
-    // Data Kecepatan
-    var speed: CGFloat = 7.0
-    var returnSpeed: CGFloat = 15.0
-    
-    // Zone
-    var surfaceY: CGFloat = 20
-    var zone1Limit: CGFloat = -200
-    var zone2Limit: CGFloat = -1000
-    var zone3Limit: CGFloat = -4000
+    let dropSpeed: CGFloat = 50
+    let reelSpeed: CGFloat = 80
 }
