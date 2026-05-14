b637becb9753924c3406d3fe49e78fcca6ae0924
Miranda <mirandautami3@gmail.com>
2026-05-14 22:43:04 +0700

    feat: add IdleState for initializing hook ready state in state machine

--- Hook/States/Game/IdleState.swift
+++ Hook/States/Game/IdleState.swift
@@ -5,4 +5,18 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+
+class IdleState: GKState {
+    weak var entity: HookEntity?
+    
+    init(entity: HookEntity) {
+        self.entity = entity
+        super.init()
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+        entity?.component(ofType: StateComponent.self)?.currentState = .idle
+        print("Hook Ready!")
+    }
+}
