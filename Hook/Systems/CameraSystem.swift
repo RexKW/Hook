064d4a199947ff05d356542edffd88139f9e6c35
Miranda <mirandautami3@gmail.com>
2026-05-14 22:51:22 +0700

    feat: add CameraSystem to synchronize camera movement with hook position

--- Hook/Systems/CameraSystem.swift
+++ Hook/Systems/CameraSystem.swift
@@ -5,4 +5,18 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+
+class CameraSystem: GKComponentSystem<CameraComponent> {
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        for component in components {
+           
+            guard let entity = component.entity,
+                  let node = entity.component(ofType: GKSKNodeComponent.self)?.node else { continue }
+            
+            let targetY = node.position.y
+            component.cameraNode.position.y = min(0, targetY)
+        }
+    }
+}
