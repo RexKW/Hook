504dae983ef886436a10bf5075a6534e2a3b4d52
Miranda <mirandautami3@gmail.com>
2026-05-14 22:40:14 +0700

    feat: Implement StateComponent with boat tier system and depth limit configuration for hook progression

--- Hook/Components/StateComponent.swift
+++ Hook/Components/StateComponent.swift
@@ -9,8 +9,19 @@ import GameplayKit
 
 class StateComponent: GKComponent {
     var currentState: HookState = .idle
+    
+    var boatTier: BoatTier = .boatLevel1
 }
 
+// Hook status
 enum HookState {
-    case idle, casting, waiting, reeling, captured
+    case idle, casting, waiting, reeling
 }
+
+// Zone limits according to ship level
+enum BoatTier: CGFloat {
+    case boatLevel1 = -200
+    case boatLevel2 = -1000
+    case boatLevel3 = -4000
+}
+
