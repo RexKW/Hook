75644c2ae2edc8f3391543b3566416dbffe7869a
Miranda <mirandautami3@gmail.com>
2026-05-14 22:42:29 +0700

    feat: add CastingState with hook casting animation and automatic transition to waiting state

--- Hook/States/Game/CastingState.swift
+++ Hook/States/Game/CastingState.swift
@@ -5,4 +5,30 @@
 //  Created by Rex Kenny Wirasantoso on 11/05/26.
 //
 
-import Foundation
+import GameplayKit
+
+class CastingState: GKState {
+    weak var entity: HookEntity?
+    
+    init(entity: HookEntity) {
+        self.entity = entity
+        super.init()
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+
+        guard let entity = entity,
+              let node = entity.component(ofType: GKSKNodeComponent.self)?.node else {
+            print("❌ CastingState: GKSKNodeComponent tidak ketemu")
+            return
+        }
+        
+        entity.component(ofType: StateComponent.self)?.currentState = .casting
+        
+        node.run(SKAction.moveTo(y: -50, duration: 0.5)) { [weak self] in
+            print("✅ Masuk ke Waiting State")
+            self?.stateMachine?.enter(WaitingState.self)
+        }
+    }
+}
+
