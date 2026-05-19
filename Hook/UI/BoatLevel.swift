4a3611f899dff0273ae0062f496d4fa2e1807313
sevet15 <stvalentino999@gmail.com>
2026-05-19 11:51:17 +0700

    Feat : game scene
    - add new fish reward and boat level swiftui

---
+++ Hook/UI/BoatLevel.swift
@@ -0,0 +1,18 @@
+//
+//  BoatLevel.swift
+//  Hook
+//
+//  Created by Steven Valentino on 19/05/26.
+//
+
+import SwiftUI
+
+struct BoatLevel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoatLevel()
+}
---
+++ Hook/UI/FishReward.swift
@@ -0,0 +1,24 @@
+//
+//  FishReward.swift
+//  Hook
+//
+//  Created by Steven Valentino on 19/05/26.
+//
+
+import SwiftUI
+
+struct FishReward: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+    }
+    
+    func xpCard(name:String, description:String) {
+        
+    }
+}
+
+#Preview {
+    FishReward()
+}
