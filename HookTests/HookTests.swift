faaf60bcce297618cd3f38f619710a21f90dadac
elvernh <87465378+elvernh@users.noreply.github.com>
2026-05-22 09:37:21 +0700

    feat & fix: added sea ambience bgm to the game, fixed some bug within the CollectionPopUpView where the fish is too big. adjusted the FishModel

---
+++ HookTests/HookTests.swift
@@ -0,0 +1,18 @@
+//
+//  HookTests.swift
+//  HookTests
+//
+//  Created by Christianto Elvern Haryanto on 22/05/26.
+//
+
+import Testing
+
+struct HookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
